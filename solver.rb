d1991f583d07d6af81e8666dafe2f81bb7e4f8e9
john <kiokoj81@gmail.com>
2023-01-14 15:26:56 +0300

    Add the FizzBuzz Method

--- solver.rb
+++ solver.rb
@@ -19,4 +19,17 @@ class Solver
     def reversed(string)
         string.reverse
     end
+
+    def fizzbuzz(number)
+        if number % 3 == 0 && number % 5 == 0
+            'FizzBuzz'
+        elsif number % 3 == 0
+            'Fizz'
+        elsif number % 5 == 0
+            'Buzz'
+        else
+            number.to_s
+        end
+    end
+
 end
